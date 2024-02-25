87fbe91000236729813c6f080b6d565ced0cb4a2
jayr <jayrsantiago206@gmail.com>
2024-02-24 20:58:46 -0500

    update prueba registro de mascota

---
+++ veterinaria/src/main/java/co/edu/uniquindio/poo/Mascota.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.poo;
+
+public record Mascota(String nombre,String especie, String raza, int edad, String genero, String color, Float peso ) {
+    
+    public Mascota{
+        assert nombre != null && !nombre.isBlank() :"El nombre de la mascota no puede ser null y tampoco puede estar vacío.";
+        assert especie != null && !especie.isBlank() :"La especie de la mascota no puede ser null y tampoco puede estar vacío.";
+        assert raza != null && !raza.isBlank() :"La raza de la mascota no puede ser null y tampoco puede estar vacío.";
+        assert edad>=0;
+        assert genero != null && !genero.isBlank() && (genero.equals("macho") || genero.equals("hembra")) : "El género de la mascota no puede ser null y tampoco puede estar vacío.";
+        assert color != null && !color.isBlank() :"El color de la mascota no puede ser null y tampoco puede estar vacío.";
+        assert peso >= 0;
+    }
+
+
+
+}
--- veterinaria/src/test/java/co/edu/uniquindio/poo/AppTest.java
+++ veterinaria/src/test/java/co/edu/uniquindio/poo/AppTest.java
@@ -7,6 +7,8 @@
  */
 package co.edu.uniquindio.poo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
@@ -17,13 +19,62 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
     private static final Logger LOG = Logger.getLogger(AppTest.class.getName());
 
-    /**
-     * Rigorous Test :-)
+       /*
+     * Prueba para validar a la mascota
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        LOG.info("Iniciado test shouldAnswerWithTrue");
-        assertTrue(true);
-        LOG.info("Finalizando test shouldAnswerWithTrue");
+    public void datosCompletos() {
+        LOG.info("Iniciado test datosCompletos");
+
+        Mascota mascota1 = new Mascota("Boby","Canis familiaris","Beagle",3,"macho", "Gris", 8.4f);
+
+        assertEquals("Boby", mascota1.nombre());
+        assertEquals("Canis familiaris", mascota1.especie());
+        assertEquals("Beagle", mascota1.raza());
+        assertEquals(3, mascota1.edad());
+        assertEquals("macho", mascota1.genero());
+        assertEquals("Gris", mascota1.color());
+        assertEquals(8.4f, mascota1.peso());
+
+        LOG.info("Los datos estan completos");
     }
+
+    /*
+     * Prueba para verificar que los datos de la mascota no sean nulos
+     */
+
+     @Test
+     public void datosNulos(){
+        LOG.info("Inicio prueba datosNulos");
+        assertThrows(Throwable.class, ()-> new Mascota(null, null, null, 0, null, null, 0f));
+        LOG.info("Error, los datos no pueden ser nulos");
+
+        
+     }
+
+     @Test
+     public void datosVacios(){
+        LOG.info("Inicio prueba datosVacios");
+        assertThrows(Throwable.class, ()-> new Mascota("", "", "", 0, "", "", 0f));
+        LOG.info("Error, faltan los datos para registrar a la mascota");
+
+     }
+
+     @Test
+     public void edadNegativa(){
+        LOG.info("Inicio prueba edadNegativa");
+        assertThrows(Throwable.class, ()-> new Mascota("Boby", "Canis familiaris", "Beagle", -3, "macho", "Gris", 8.4f));
+        LOG.info("Error, la edad no puede ser negativa");
+     }
+
+     @Test
+     public void pesoNegativo(){
+        LOG.info("Inicio prueba pesoNegativo");
+        assertThrows(Throwable.class, ()-> new Mascota("Boby", "Canis familiaris", "Beagle", 3, "macho", "Gris", -8.4f));
+        LOG.info("Error, el peso no puede ser negativo");
+     }
+
+ 
+
+     
 }
